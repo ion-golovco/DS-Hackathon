a4a5049c6396ca2433133dca9f42dca5ccda8159
Radu <cazacuraduic@gmail.com>
2023-03-04 16:07:59 +0200

    Radu

--- server/main.js
+++ server/main.js
@@ -1,5 +1,10 @@
-import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/api/links';
+import { Meteor } from "meteor/meteor";
+import { LinksCollection } from "/imports/api/links";
+import { MongoClient } from "mongodb";
+
+const url =
+  "mongodb+srv://radu:j0fFCHRGolqlI1Mw@cluster0.6ogxery.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(url, { useNewUrlParser: true });
 
 async function insertLink({ title, url }) {
   await LinksCollection.insertAsync({ title, url, createdAt: new Date() });
@@ -7,25 +12,25 @@ async function insertLink({ title, url }) {
 
 Meteor.startup(async () => {
   // If the Links collection is empty, add some data.
-  if (await LinksCollection.find().countAsync() === 0) {
+  if ((await LinksCollection.find().countAsync()) === 0) {
     await insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://www.meteor.com/tutorials/react/creating-an-app',
+      title: "Do the Tutorial",
+      url: "https://www.meteor.com/tutorials/react/creating-an-app",
     });
 
     await insertLink({
-      title: 'Follow the Guide',
-      url: 'https://guide.meteor.com',
+      title: "Follow the Guide",
+      url: "https://guide.meteor.com",
     });
 
     await insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com',
+      title: "Read the Docs",
+      url: "https://docs.meteor.com",
     });
 
     await insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com',
+      title: "Discussions",
+      url: "https://forums.meteor.com",
     });
   }
 });
